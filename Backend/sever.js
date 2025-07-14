2093d73f5508f51672438846fbe71289e13f9ba9
divya-pasupuleti05 <divyapasupuleti2720@gmail.com>
2025-07-14 18:10:50 +0530

    added sever.js to backend

---
+++ Backend/sever.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router("userdata.json");
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname), // Serve static files from current folder
+});
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(3000, () => {
+  console.log("JSON Server is running with static support");
+});
